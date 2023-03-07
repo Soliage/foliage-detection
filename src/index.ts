7244481b5c8c52dd8fc9cd1db134149f3d868c58
Caronfire <caron.schaller@gmail.com>
2023-03-07 10:30:46 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ const { exec } = require('node:child_process');
 
 Dotenv.config()
 
+const ORACLE_ADDRESS = 'CQ9sBC6dELeBFPtRLS3YM9Zw6K91xDG1drnxzcYG9uYS'
 const PROGRAM_ADDRESS = 'ChT1B39WKLS8qUrkLvFDXMhEJ4F1XZzwUNHUt4AU9aVa'
 const PROGRAM_DATA_ADDRESS = 'Ah9K7dQ8EHaZqcAsgBW8w37yN2eAy3koFmUn4x3CJtod'
 
